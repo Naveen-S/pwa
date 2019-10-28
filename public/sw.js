6cf847eb692ce35e68b97fd9a51cf2e97b19ee41
naveen <naveentejas1109@gmail.com>
2019-10-28 14:54:19 +0530

    Caching multiple files using cache.addAll() method.

--- public/sw.js
+++ public/sw.js
@@ -2,10 +2,19 @@ self.addEventListener('install', (e) => {
     console.log('[Service Worker] Installing service worker! ', e);
     e.waitUntil(caches.open('static').then(cache => {
         console.log('[Service Worker] Pre caching...')
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
-
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/src/js/app.js',
+            '/src/js/feed.js',
+            '/src/js/material.min.js',
+            '/src/css/app.css',
+            '/src/css/feed.css',
+            '/src/images/main-image.jpg',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ])
     }))
 })
 
