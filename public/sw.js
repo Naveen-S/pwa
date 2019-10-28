d68fa8c06cf75c55bf1b1801b6414f174cb47bb1
naveen <naveentejas1109@gmail.com>
2019-10-28 14:39:04 +0530

    Caching files for offline access.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,12 @@
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Installing service worker! ', e);
+    e.waitUntil(caches.open('static').then(cache => {
+        console.log('[Service Worker] Pre caching...')
+        cache.add('/');
+        cache.add('/index.html');
+        cache.add('/src/js/app.js');
+
+    }))
 })
 
 self.addEventListener('activate', (e) => {
@@ -13,5 +20,14 @@ self.addEventListener('fetch', (e) => {
     // To not respond anything [Intercept and block fetch requests]
     // e.respondWith(null);
 
-    e.respondWith(fetch(e.request));
+    // e.respondWith(fetch(e.request));
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            if(response) {
+                return response;
+            } else {
+                return fetch(e.request);
+            }
+        })
+    );
 })
